0bd1679f2f25bfe8a8bd5bcb2b44edda0141313b
nelyher98 <ClaraHer98@gmail.com>
2018-12-12 19:08:38 -0600

    Hi

--- WindowsFormsApplication1/VerPlaca.Designer.cs
+++ WindowsFormsApplication1/VerPlaca.Designer.cs
@@ -50,8 +50,9 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.panel3);
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(599, 46);
+            this.panel1.Size = new System.Drawing.Size(799, 57);
             this.panel1.TabIndex = 28;
             // 
             // label1
@@ -59,18 +60,20 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(94, 9);
+            this.label1.Location = new System.Drawing.Point(125, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 25);
+            this.label1.Size = new System.Drawing.Size(96, 31);
             this.label1.TabIndex = 10;
             this.label1.Text = "Placas";
             // 
             // panel3
             // 
             this.panel3.BackgroundImage = global::Autolavado.Properties.Resources.Logopaginas;
-            this.panel3.Location = new System.Drawing.Point(13, 4);
+            this.panel3.Location = new System.Drawing.Point(17, 5);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(75, 42);
+            this.panel3.Size = new System.Drawing.Size(100, 52);
             this.panel3.TabIndex = 0;
             // 
             // panel2
@@ -81,18 +84,20 @@
             this.panel2.Controls.Add(this.BtnMod);
             this.panel2.Controls.Add(this.panel4);
             this.panel2.Controls.Add(this.btnSalirProp);
-            this.panel2.Location = new System.Drawing.Point(-1, 45);
+            this.panel2.Location = new System.Drawing.Point(-1, 55);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(599, 322);
+            this.panel2.Size = new System.Drawing.Size(799, 396);
             this.panel2.TabIndex = 29;
             // 
             // BtnMenu
             // 
             this.BtnMenu.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.BtnMenu.Image = global::Autolavado.Properties.Resources.button;
-            this.BtnMenu.Location = new System.Drawing.Point(433, 260);
+            this.BtnMenu.Location = new System.Drawing.Point(582, 321);
+            this.BtnMenu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.BtnMenu.Name = "BtnMenu";
-            this.BtnMenu.Size = new System.Drawing.Size(86, 32);
+            this.BtnMenu.Size = new System.Drawing.Size(115, 39);
             this.BtnMenu.TabIndex = 16;
             this.BtnMenu.Text = "Menú";
             this.BtnMenu.UseVisualStyleBackColor = true;
@@ -102,9 +107,10 @@
             // 
             this.BtnNuevo.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.BtnNuevo.Image = global::Autolavado.Properties.Resources.button;
-            this.BtnNuevo.Location = new System.Drawing.Point(181, 260);
+            this.BtnNuevo.Location = new System.Drawing.Point(241, 320);
+            this.BtnNuevo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.BtnNuevo.Name = "BtnNuevo";
-            this.BtnNuevo.Size = new System.Drawing.Size(86, 32);
+            this.BtnNuevo.Size = new System.Drawing.Size(115, 39);
             this.BtnNuevo.TabIndex = 15;
             this.BtnNuevo.Text = "Nuevo";
             this.BtnNuevo.UseVisualStyleBackColor = true;
@@ -114,9 +120,10 @@
             // 
             this.BtnMod.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.BtnMod.Image = global::Autolavado.Properties.Resources.button;
-            this.BtnMod.Location = new System.Drawing.Point(55, 260);
+            this.BtnMod.Location = new System.Drawing.Point(73, 320);
+            this.BtnMod.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.BtnMod.Name = "BtnMod";
-            this.BtnMod.Size = new System.Drawing.Size(86, 32);
+            this.BtnMod.Size = new System.Drawing.Size(115, 39);
             this.BtnMod.TabIndex = 14;
             this.BtnMod.Text = "Modificar";
             this.BtnMod.UseVisualStyleBackColor = true;
@@ -126,9 +133,10 @@
             // 
             this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(50)))), ((int)(((byte)(56)))));
             this.panel4.Controls.Add(this.DataPlacas);
-            this.panel4.Location = new System.Drawing.Point(27, 10);
+            this.panel4.Location = new System.Drawing.Point(36, 12);
+            this.panel4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(536, 244);
+            this.panel4.Size = new System.Drawing.Size(715, 300);
             this.panel4.TabIndex = 13;
             // 
             // DataPlacas
@@ -136,20 +144,21 @@
             this.DataPlacas.AllowUserToAddRows = false;
             this.DataPlacas.AllowUserToDeleteRows = false;
             this.DataPlacas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DataPlacas.Location = new System.Drawing.Point(23, 19);
+            this.DataPlacas.Location = new System.Drawing.Point(31, 23);
+            this.DataPlacas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.DataPlacas.Name = "DataPlacas";
             this.DataPlacas.ReadOnly = true;
-            this.DataPlacas.Size = new System.Drawing.Size(493, 198);
+            this.DataPlacas.Size = new System.Drawing.Size(657, 244);
             this.DataPlacas.TabIndex = 0;
             // 
             // btnSalirProp
             // 
             this.btnSalirProp.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
             this.btnSalirProp.Image = global::Autolavado.Properties.Resources.button1;
-            this.btnSalirProp.Location = new System.Drawing.Point(307, 260);
-            this.btnSalirProp.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSalirProp.Location = new System.Drawing.Point(409, 320);
+            this.btnSalirProp.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSalirProp.Name = "btnSalirProp";
-            this.btnSalirProp.Size = new System.Drawing.Size(86, 32);
+            this.btnSalirProp.Size = new System.Drawing.Size(115, 39);
             this.btnSalirProp.TabIndex = 12;
             this.btnSalirProp.Text = "Eliminar";
             this.btnSalirProp.UseVisualStyleBackColor = true;
@@ -157,12 +166,13 @@
             // 
             // VerPlaca
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(596, 366);
+            this.ClientSize = new System.Drawing.Size(795, 450);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "VerPlaca";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "VerPlaca";
